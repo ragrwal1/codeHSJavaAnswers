48c2fc79acf10a1268625c8ebc1633c8d8415bf8
Rohan Agrawal <66571804+ragrwal1@users.noreply.github.com>
2021-08-11 09:30:42 -0700

    Create 1.1 Why Programming, Why Java?.java

---
+++ Unit 1, Primitive Types/1.1 Why Programming, Why Java?.java
@@ -0,0 +1,114 @@
+1.1.5 Welcome Program
+ 
+public class Welcome 
+{
+    public static void main(String[] args)
+    {
+          System.out.println("My name is Rohan ");
+      System.out.println("I like computers");
+    } 
+ 
+}
+ 
+ 
+1.1.6 ASCII Art
+ 
+public class Art 
+{
+    public static void main(String[] args)
+    {
+       System.out.println("   /)___");
+       System.out.println("  /     O|___");
+       System.out.println(" /           )");
+       System.out.println("/       )___/");
+    }
+ 
+}
+ 
+1.1.7
+ 
+public class TomatoEssay
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        System.out.println("Is a tomato a fruit or a vegetable?");
+        System.out.print("While biologically speaking a tomato");
+        System.out.println(" is a fruit, legally speaking it is a vegetable!");
+        System.out.print("In Supreme Court Case Nix v. Hedden, the Court");
+        System.out.print(" ruled that tomatoes should be classified as a vegetable");
+        System.out.print(" under U.S Customs regulations because they are consumed");
+        System.out.println(" more like a vegetable than a fruit, and should be taxed as such.");
+        System.out.print("When tomatoes are shipped into the U.S, they are now taxed as vegetables");
+        System.out.println(" even though their anatomy suggests otherwise.");
+    }
+}
+ 
+1.1.8 Heating Up
+ 
+ 
+//copy paste here
+ 
+public class MakingPopcorn
+ 
+{
+ 
+public static void main(String[] args)
+ 
+{
+ 
+System.out.println("3 Ways to Heat Anything");
+ 
+System.out.println("Each of the 3 methods below is an example of the three ways that heat can be transferred.");
+ 
+System.out.println("");
+ 
+System.out.println("Conduction");
+ 
+System.out.println("Conduction is heat transfer through matter. Metals conduct heat well, but air does not. This is a direct contact type of heat transfer.");
+ 
+System.out.println("");
+ 
+System.out.println("Convection");
+ 
+System.out.println("Convection is heat transfer by the movement of mass from one place to another. It can take place only in liquids and gases. Heat gained by conduction or radiation is moved by convection.");
+ 
+System.out.println("");
+ 
+System.out.println("Radiation");
+ 
+System.out.println("Radiation can transfer heat through the relative emptiness of space. All other forms of heat transfer require motion of molecules like air or water to move heat.");
+ 
+}
+ 
+}
+ 
+1.1.9 Personal Timeline
+ 
+public class Timeline
+                {
+                    public static void main(String[] args)
+                    {
+                        System.out.println("Age  | M/YY Description");
+                        System.out.println("7    | ");
+                        System.out.println("     | 5/08 - lorem ipsum");
+                        System.out.println("     | ");
+                        System.out.println("     | ");
+                        System.out.println("9    | ");
+                        System.out.println("     | ");
+                        System.out.println("     | ");
+                        System.out.println("     | ");
+                        System.out.println("11   | 1/14 - amet sit dolor");
+                        System.out.println("     | ");
+                        System.out.println("     | 6/14 - consectetur ");
+                        System.out.println("     | ");
+                        System.out.println("13   | 2/16 - adipiscing elit, ");
+                        System.out.println("     | 3/16 - sed do eiusmod");
+                        System.out.println("     | 9/16 - tempor incididunt ut labore");
+                        System.out.println("     | ");
+                        System.out.println("15   | ");
+                        System.out.println("     | ");
+                        System.out.println("     | ");                  }
+            }
+ 
+ 
